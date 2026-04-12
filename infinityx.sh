7bec7220fc8844c4c08e3e438c493bebfb4b78b2
ubaidraye <ubaidraye706@gmail.com>
2026-04-13 01:15:37 +0530

    script: infinityx.sh: do not extract vendor

--- infinityx.sh
+++ infinityx.sh
@@ -6,7 +6,6 @@ rm -rf device/motorola
 rm -rf kernel/motorola
 rm -rf vendor/motorola
 rm -rf .repo/local_manifests
-rm -rf *.zip
 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 /opt/crave/resync.sh || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
@@ -20,9 +19,8 @@ git clone https://github.com/ubaidraye/android_kernel_motorola_sm8475 -b lineage
 git clone https://github.com/ubaidraye/android_kernel_motorola_sm8475-modules -b lineage-23.2 kernel/motorola/sm8475-modules
 git clone https://github.com/ubaidraye/android_kernel_motorola_sm8475-devicetrees -b lineage-23.2 kernel/motorola/sm8475-devicetrees
 
-
 # clone vendor
-git clone https://github.com/themuppets/proprietary_vendor_motorola_eqs vendor/motorola/eqs --depth=1
+git clone https://github.com/ubaidraye/proprietary_vendor_motorola_eqs -b lineage-23.2 vendor/motorola/eqs --depth=1
 git clone https://github.com/ubaidraye/proprietary_vendor_motorola_sm8475-common -b lineage-23.2 vendor/motorola/sm8475-common --depth=1
 
 # clone hardware source
@@ -34,23 +32,9 @@ cd kernel/motorola/sm8475
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 cd ../../..
 
-
-# get A15 Vendor
-wget https://mirrors-obs-1.lolinet.com/firmware/lenomola/2022/eqs/official/RETAIL/EQS_RETAIL_15_V1SQS35H.58-10-8-1_subsidy_DEFAULT_regulatory_DEFAULT_cid50_CFC.xml.zip
-
-unzip EQS_RETAIL_15_V1SQS35H.58-10-8-1*.zip -d device/motorola/eqs/A15-vendor
-
-# extract new vendor
-cd device/motorola/eqs
-./setup-makefiles.py
-./extract-files.py A15-vendor
-cd ../../..
-
 export BUILD_USERNAME=RayeUB
 export BUILD_HOSTNAME=crave
 export DISABLE_STUB_VALIDATION=true
-export INFINITY_MAINTAINER=RayeUB
-export TARGET_HAS_UDFPS=true
 
 . build/envsetup.sh
 lunch infinity_eqs-userdebug
